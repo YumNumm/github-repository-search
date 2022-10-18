541e92b0597e73d0a1f76165b57db8cb903b5287
YumNumm <onory0917@gmail.com>
2022-10-18 23:45:12 +0900

    add: i18nへの対応

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,42 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_enginner_codecheck/ui/app.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:github_repository_search/provider/language_colors.dart';
+import 'package:github_repository_search/provider/shared_preferences.dart';
 import 'package:logger/logger.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'i18n/translations.g.dart';
+import 'ui/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  LocaleSettings.useDeviceLocale();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent, // transparent status bar
+    ),
+  );
   PlatformDispatcher.instance.onError = (error, stackTrace) {
     Logger().e(error, stackTrace);
     return true;
   };
+  // LanguageColorsを読み込み
+  final languageColors = await loadLanguageColors();
+  // SharedPreferenceを初期化
+  final prefs = await SharedPreferences.getInstance();
+
   runApp(
     ProviderScope(
       observers: const [],
+      overrides: [
+        languageColorsProvider.overrideWithValue(languageColors),
+        sharedPreferencesProvider.overrideWithValue(prefs),
+      ],
       child: DevicePreview(
-        builder: (context) => const App(),
+        builder: (context) => TranslationProvider(child: const App()),
       ),
     ),
   );
---
+++ lib/ui/page/component/setting/setting_section.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+/// 設定のセクションを作成するWidget
+/// ref: https://github.com/EQMonitor/EQMonitor/blob/main/lib/ui/view/widget/setting/setting_section.dart
+class SettingsSection extends StatelessWidget {
+  const SettingsSection({
+    super.key,
+    required this.title,
+    required this.children,
+  });
+
+  final String? title;
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    final t = Theme.of(context);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        if (title != null)
+          Container(
+            padding: const EdgeInsets.only(top: 16, bottom: 10, left: 20),
+            child: Text(
+              title!,
+              style: TextStyle(
+                color: t.colorScheme.primary,
+              ),
+            ),
+          ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: children,
+        ),
+      ],
+    );
+  }
+}
